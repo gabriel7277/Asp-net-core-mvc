cd1185bd75d54d2ce859e91ff81b09a22c39e6ca
gabriel7277 <gabriel727fernandes@gmail.com>
2023-12-16 21:26:43 -0300

    First controller and Razor page tests

--- SalesWebMvc/Controllers/HomeController.cs
+++ SalesWebMvc/Controllers/HomeController.cs
@@ -17,7 +17,8 @@ namespace SalesWebMvc.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
+            ViewData["Message"] = "Salles web MVC App";
+            ViewData["Name"] = "Gabriel";
 
             return View();
         }
