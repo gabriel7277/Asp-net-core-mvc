577de62610a25826d0f5142d8972becdb3df6ba0
gabriel7277 <gabriel727fernandes@gmail.com>
2024-02-09 22:13:15 -0300

    Update seller and custom service exception

--- SalesWebMvc/Controllers/SellersController.cs
+++ SalesWebMvc/Controllers/SellersController.cs
@@ -6,6 +6,8 @@ using System.Linq;
 using System.Threading.Tasks;
 using SalesWebMvc.Models;
 using SalesWebMvc.Models.ViewModels;
+using SalesWebMvc.Services.Exceptions;
+
 namespace SalesWebMvc.Controllers
 {
     public class SellersController : Controller
@@ -76,6 +78,47 @@ namespace SalesWebMvc.Controllers
             }
             return View(obj);
 
+        }
+
+        public IActionResult Edit(int ? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var obj = _sellerService.FindById(id.Value);
+            if(obj == null)
+            {
+                return NotFound();
+            }
+
+            List<Department> departments = _departmentService.FindAll();
+            SellerFormViewModel viewModel = new SellerFormViewModel { Seller = obj, Departments = departments };
+            return View(viewModel);
+        }
+        
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit (int id, Seller seller)
+        {
+            if (id != seller.Id)
+            {
+                return BadRequest();
+            }
+            try
+            {
+                _sellerService.Update(seller);
+                return RedirectToAction(nameof(Index));
+            }
+            catch (NotFoundException)
+            {
+                return NotFound();
+            }
+            catch (DbConcurrencyException)
+            {
+                return BadRequest();
+            }
         }
     }
 }
---
+++ SalesWebMvc/Services/Exceptions/DbConcurrencyException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+
+namespace SalesWebMvc.Services.Exceptions
+{
+    public class DbConcurrencyException : ApplicationException
+    {
+        public DbConcurrencyException (string message) : base(message)
+        {
+
+        }
+    } 
+}
